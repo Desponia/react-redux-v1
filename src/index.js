b760ede8213857acd76d41e010166acf9199972e
desponia <coalcellar@sk.com>
2019-01-03 20:21:57 +0900

    change app component function to class for using state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyAZTO-s1IBQLTjAWqvBAb-iiGGK1mW4u1U'
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {videos: []}
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            // es6 key와 value 이름이 같은 경우에만 아래처럼 쓸 수 있음.
+            this.setState({videos})
+            // this.setState({videos: data})
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )
+    }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
